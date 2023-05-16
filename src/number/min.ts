68708c111de7500ef37cfcab4097b80ceab18456
David Kander <kanders.davids@gmail.com>
2023-05-16 07:07:31 +0300

    `1.7.0`

--- src/number/min.ts
+++ src/number/min.ts
@@ -1,5 +1,13 @@
 export function min(
   numbers: readonly number[],
+  percentile?: number,
 ): number {
-  return Math.min(...numbers)
+  if (percentile) {
+    if (percentile <= 0 || percentile > 100) throw 'percentile out of bounds'
+    let sorted = numbers.slice().sort((a, b) => b - a)
+    let index = Math.ceil((percentile / 100) * sorted.length) - 1
+    return sorted[index]!
+  } else {
+    return Math.min(...numbers)
+  }
 }
