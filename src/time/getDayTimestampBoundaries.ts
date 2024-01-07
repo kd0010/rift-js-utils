1f3d68f24fb3f08be1564f7a15d993197f971887
David Kander <kanders.davids@gmail.com>
2024-01-07 15:25:58 +0200

    fix

--- src/time/getDayTimestampBoundaries.ts
+++ src/time/getDayTimestampBoundaries.ts
@@ -7,10 +7,11 @@ import { ensureTimestampFormat } from './ensureTimestampFormat'
 export function getDayTimestampBoundaries(
   timestamp: number,
   /** UTC`+x` or UTC`-x`; `x` being `timezoneOffsetHours`. @default new Date(timestamp).getTimezoneOffset() / 60  */
-  timezoneOffsetHours: number=0,
+  timezoneOffsetHours?: number,
 ): [number, number] {
   const ts = ensureTimestampFormat('millisecond', timestamp)
-  if (Date != null) timezoneOffsetHours = -1 * new Date(timestamp).getTimezoneOffset() / 60
+  if (Date != null) timezoneOffsetHours ??= -1 * new Date(timestamp).getTimezoneOffset() / 60
+  else timezoneOffsetHours = 0
   const tzOffsetTs = ts + timezoneOffsetHours * hourMilliseconds // convert to ms
 
   let currentDayMs = tzOffsetTs % dayMilliseconds
