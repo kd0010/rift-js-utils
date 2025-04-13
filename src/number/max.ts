b385f668167185d40089190b794ec7a2b68c16e1
kd0010 <65524828+kd0010@users.noreply.github.com>
2025-04-13 21:05:30 +0300

    fix numbers: `min`, `max`

--- src/number/max.ts
+++ src/number/max.ts
@@ -3,9 +3,9 @@ export function max(
   percentile?: number,
 ): number {
   if (percentile) {
-    if (percentile <= 0 || percentile > 100) throw 'percentile out of bounds'
+    if (percentile < 0 || percentile >= 100) throw 'percentile out of bounds'
     let sorted = numbers.slice().sort((a, b) => a - b)
-    let index = Math.ceil((percentile / 100) * sorted.length) - 1
+    let index = Math.ceil(((100 - percentile) / 100) * sorted.length) - 1
     return sorted[index]!
   } else {
     return Math.max(...numbers)
